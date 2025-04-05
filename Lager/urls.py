398cf9e02a6286ccdefbd22f8ce997afa73e6efb
Mika Griebsch <mika.griebsch@gmail.com>
2025-04-05 08:42:07 +0200

    Header mit Home Anzeige

--- Lager/urls.py
+++ Lager/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('orders/', views.order_list, name='order_list'),
     path('oldorders/', views.old_order_list, name='old_order_list'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+
+    path('home/', views.home, name='home'),
 ]
--- Lager/views.py
+++ Lager/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Order
+from django.utils import timezone
+from datetime import datetime
 
 def order_list(request):
     orders = Order.objects.filter(status='pending').order_by('-order_date')
@@ -12,3 +14,21 @@ def old_order_list(request):
 def order_detail(request, order_id):
     order = get_object_or_404(Order, pk=order_id)
     return render(request, 'order_detail.html', {'order': order})
+
+def home(request):
+    # Anzahl offener Bestellungen
+    open_orders_count = Order.objects.filter(status='pending').count()
+    
+    # Anzahl heute abgeschlossener Bestellungen
+    today = timezone.now().date()
+    completed_today_count = Order.objects.filter(
+        status='completed',
+        order_date__date=today
+    ).count()
+    
+    context = {
+        'open_orders_count': open_orders_count,
+        'completed_today_count': completed_today_count,
+    }
+    
+    return render(request, 'home.html', context)
